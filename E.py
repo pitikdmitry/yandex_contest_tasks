26c2070fb17b614a9d583695c0b02477ab37dfe6
pitikdmitry <pitik.dmitry@mail.ru>
2018-11-11 22:56:11 +0300

    69 test

--- E.py
+++ E.py
@@ -1,41 +1,49 @@
+def delete_dubl(arr):
+
+    new_arr = []
+    new_arr.append(arr[0])
+    previous = arr[0]
+
+    for i in range(1, len(arr)):
+        if arr[i] == previous:
+            continue
+        else:
+            new_arr.append(arr[i])
+            previous = arr[i]
+    return new_arr
+
+
 def primfacs(n):
     i = 2
     primfac = []
 
     while i * i <= n:
-       while n % i == 0:
-           primfac.append(i)
-           primfac.append(n)
-           n = n / i
-       i = i + 1
+        while n % i == 0:
+            primfac.append(i)
+            primfac.append(int(n))
+            n = n / i
+        i = i + 1
     if n > 1:
         primfac.append(int(n))
 
-    arr = sorted(primfac)
-    previous = arr[0]
-
-    new_arr = []
-    new_arr.append(arr[0])
-
-    for i in range(1, len(arr)):
-        if arr[i] == previous:
-            continue
-    else:
-        new_arr.append(arr[i])
-        previous = arr[i]
-
-    return new_arr
+    return primfac
 
 
 def isSubsetSum(set, n, mul, k, prim_arr):
 
-    subset = ([[None for i in range(mul + 1)]
+    subset = ([[None for i in range(len(prim_arr) + 2)]
               for i in range(n + 2)])
     # If sum is not 0 and set is empty,
     # then answer is false
-    for i in range(mul + 1):
-        subset[0][i] = None
+    subset[0][0] = None
+    subset[0][1] = None
+    j = 0
+    indexes = {}
+    for i in range(2, len(prim_arr) + 2):
+        indexes[prim_arr[j]] = i
+        subset[0][i] = prim_arr[j]
         subset[1][i] = None
+        j += 1
 
     # If sum is 0, then answer is true
     for i in range(n + 1):
@@ -44,19 +52,23 @@ def isSubsetSum(set, n, mul, k, prim_arr):
 
     # Fill the subset table in botton up manner
     for i in range(2, n + 2):
-        for j in range(2, mul + 1):
-
-            if j < set[i - 2]:
+        for j in range(2, len(prim_arr) + 2):
+            new_j = subset[0][j]
+            if new_j < set[i - 2]:
                 subset[i][j] = subset[i - 1][j]
-            if j == set[i - 2]:
+            elif new_j == set[i - 2]:
                 subset[i][j] = [i - 2]
-
-            if j > set[i - 2]:
-                j_index = j / set[i - 2]
+            elif new_j > set[i - 2]:
+                j_index = new_j / set[i - 2]
                 if j_index.is_integer():
                     j_index = int(j_index)
 
-                    if subset[i - 1][j_index] is not None and len(subset[i - 1][j_index]) > 0:
+                    j_index = indexes.get(j_index)
+                    # j_index = indexes[j_index]
+                    #
+                    if j_index is None:
+                        subset[i][j] = subset[i - 1][j]
+                    elif subset[i - 1][j_index] is not None and len(subset[i - 1][j_index]) > 0:
                         new_arr = subset[i - 1][j_index].copy()
                         new_arr.append(i - 2)
                         subset[i][j] = new_arr
@@ -65,24 +77,12 @@ def isSubsetSum(set, n, mul, k, prim_arr):
                 else:
                     subset[i][j] = subset[i - 1][j]
 
-            if j == mul:
+            if j == len(prim_arr) + 1:
                 result = checkSolution(set, subset[i][j], mul, k)
                 if result:
                     return result
 
-
-
-    # uncomment this code to print table
-    # for i in range(n+1):
-    #     for j in range(mul+1):
-    #         print(subset[i][j], end=" ")
-    #     print()
-
-
-    # #find_solution
-    # for i in range(len(subset)):
-    #     last_column_arr = subset[i][len(subset[i]) - 1]
-    #     checkSolution(set, last_column_arr, mul, k)
+    print()
 
 
 def checkSolution(set, arr, mul, k):
@@ -127,11 +127,6 @@ for idx, el in enumerate(arr):
     else:
         new_arr.append(element)
 
-all_multiplies = set()
-for i in range(len(new_arr)):
-    for j in range(i, len(new_arr)):
-        all_multiplies.add(new_arr[i] * new_arr[j])
-
 n = len(new_arr)
 
 if m == 0:
@@ -162,6 +157,9 @@ elif k == 1:
             break
 else:
     prim_arr = primfacs(m)
+    prim_arr.extend(new_arr)
+    prim_arr = sorted(prim_arr)
+    prim_arr = delete_dubl(prim_arr)
     res = isSubsetSum(new_arr, n, m, k, prim_arr)
     for idx in res:
         print(idx + 1, end=" ")
